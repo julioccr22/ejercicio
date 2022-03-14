452f8faaf13a66de14b3ba83b826366c30fa1491
julioccr22 <101595236+julioccr22@users.noreply.github.com>
2022-03-14 14:05:43 -0500

    Update principal.py
    Julio Campo R - Volumen de un cubo

--- principal.py
+++ principal.py
@@ -2,10 +2,11 @@ import math
 def area_cuadrado(lado):
 	area=lado**2
 	print(f"El area es {area}")
-
-def rhombusArea(d1,d2): 
-    area = (d1*d2)/2
-    return area
 	
 area_cuadrado(6) 
 	
+def volcub(a):
+    volC = a * a * a
+    print(f"El volúmen del cubo es: {volC} cm")
+
+volcub(5)
